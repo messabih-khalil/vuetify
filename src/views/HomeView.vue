fcd5649d40e8f4af1188c037351cf3ed9da21811
messabih-khalil <msbih.khalil@gmail.com>
2023-01-25 12:39:26 +0100

    buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-btn class="bg-red text-white"> Clikck </v-btn>
+
+    <v-btn flat class="bg-black">
+      <v-icon icon="mdi-delete-empty-outline"></v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
+  name: "HomeView",
 });
 </script>
